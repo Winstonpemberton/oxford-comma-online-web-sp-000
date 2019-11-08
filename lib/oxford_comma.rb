23ee0e28ff15c9d990cf332912f4c797f1c88e11
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-08 22:43:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,9 @@ def oxford_comma(array)
   # if array.size == 0
   #   array.join
   # elsif array.size == 1
-  array.join(" and ")
+#  array.join(" and ")
   #end
+return array.to_s if array.nil? or array.length <= 1
+array[-1] = "and #{array[-1]}"
+array.join(', ')
 end
